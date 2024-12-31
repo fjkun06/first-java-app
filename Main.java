3d48f7ea7fa6b2aab7e785c9d1969bdddc12dba3
fjkun06 <fjkun06@gmail.com>
2024-12-31 13:24:11 +0100

    completed variables lesson

--- Main.java
+++ Main.java
@@ -1,6 +1,15 @@
 public class Main {
   public static void main(String[] args) {
+    String sex = "male";
+    int age = 3 * 5;
+    age = 24;
+    int a, b, c;
+    a = b = c = 87;
+    int x = 4, y = 8, z = 9;
+    System.out.println(x + y + z);
+    System.out.println(a+ b + c);
     System.out.println("Hello XXX");
     System.out.println("Hello aaa");
+    System.out.println(age + " " + sex);
   }
 }
