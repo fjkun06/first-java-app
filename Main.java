3395d29a1d6812b375e54495a25c187903c5bff6
fjkun06 <fjkun06@gmail.com>
2024-12-31 14:48:04 +0100

    completed ternary operator

--- Main.java
+++ Main.java
@@ -9,6 +9,7 @@ public class Main {
     a = b = c = 87;
     int x = 4, y = 8, z = 9;
     double age2 = age;
+    int randomNum = (int)(Math.random() * 101); 
     System.out.println(x + y + z);
     System.out.println(a+ b + c);
     System.out.println("Hello XXX");
@@ -16,5 +17,16 @@ public class Main {
     System.out.println("It\'s alright! oder \\ nicht?");
     System.out.println("Call me \"Madhaus\".");
     System.out.println(age2 + " " +  sex + addString);
+    System.out.println((int) Math.sqrt(16));
+    System.out.println((int) randomNum);
+
+    System.out.println("Comparing " + a + " and " + x);
+    if (a > x) System.out.println(a + " is greater");
+    else if (age < x) System.out.println(x + " is greater");
+    else
+      System.out.println(x + " is greater");
+     
+    String res = (age > z) ? "young" : "old ";
+    System.out.println(res);
   }
 }
