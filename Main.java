c801ddd87da03eeda7eea1252c7a13da264793a3
fjkun06 <fjkun06@gmail.com>
2024-12-31 14:27:59 +0100

    Completed strings

--- Main.java
+++ Main.java
@@ -1,15 +1,20 @@
 public class Main {
   public static void main(String[] args) {
     String sex = "male";
+    String serc = "Man ";
+    String addString = sex.concat(serc);
     int age = 3 * 5;
     age = 24;
     int a, b, c;
     a = b = c = 87;
     int x = 4, y = 8, z = 9;
+    double age2 = age;
     System.out.println(x + y + z);
     System.out.println(a+ b + c);
     System.out.println("Hello XXX");
     System.out.println("Hello aaa");
-    System.out.println(age + " " + sex);
+    System.out.println("It\'s alright! oder \\ nicht?");
+    System.out.println("Call me \"Madhaus\".");
+    System.out.println(age2 + " " +  sex + addString);
   }
 }
