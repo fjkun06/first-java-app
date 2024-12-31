07b53c2d33ab4f42ad762c4562f1ecc365bbd892
fjkun06 <fjkun06@gmail.com>
2024-12-31 17:48:22 +0100

    printed psychadelic

--- TrianglePattern.java
+++ TrianglePattern.java
@@ -4,23 +4,49 @@ public class TrianglePattern {
     System.out.println("no newline::::");
     System.out.println("shuuuuu");
 
-    for (int i = 0; i < 4; i++) {
-      System.out.println(i + ":+");
-    }
-    for (int i = 1; i <= 4; i++) {
-      for (int j = 1; j <= i; j++) {
-        System.out.print("*");
+    int numOfTimes = 10;
+    int halfLife = numOfTimes / 2;
+    System.out.println("\n");
+      
+
+    for (int i = 0; i < numOfTimes; i++) {
+    
+      if (i <= 4) {
+             for (int j = halfLife; j > i; j--) {
+               if (i != 4)
+                 System.out.print("*");
+       else System.out.print("\u001B[31m*\u001B[0m");
+      }
+       for (int j = 1; j < (halfLife + i)*2; j++) {
+        System.out.print(" ");
+      }
+              for (int j = halfLife; j > i; j--) {
+         if (i != 4)
+                 System.out.print("*");
+       else System.out.print("\u001B[31m*\u001B[0m");
       }
       System.out.println("\n");
+      }
+
+      else {
+        for (int j = 0; j <= i - halfLife; j++) {
+             if (i != 5)
+                 System.out.print("*");
+               else
+                 System.out.print("\u001B[34m*\u001B[0m");
+      }
+       for (int j = 1; j < (3*halfLife - (i + 1))*2; j++) {
+        System.out.print(" ");
+      }
+          for (int j = 0; j <= i - halfLife; j++) {
+         if (i != 5)
+                 System.out.print("*");
+               else
+                 System.out.print("\u001B[34m*\u001B[0m");
+      }
+      System.out.println("\n");
+      }
     }
 
-    int[] A = { 1, 2, 3 };
-    int[] B = { 4, 5, 6 };
-    int dotProduct = 0;
-    for (int i = 0; i < A.length; i++) {
-      dotProduct += A[i] * B[i];
-    }
-
-    System.out.println(dotProduct);
   }
 }
